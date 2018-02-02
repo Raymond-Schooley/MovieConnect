7aae0beeb4277d8832976c9638af8125b5470990
walkhh <36055790+walkhh@users.noreply.github.com>
2018-02-02 13:16:10 -0800

    fixed parse bug

--- create-movies_info_raw.py
+++ create-movies_info_raw.py
@@ -2,11 +2,11 @@ import csv
 import os
 
 #MUST CHANGE THESE
-imdbFile = 'C:\\...\\title.basics.tsv'
+imdbFile = 'C:\\Users\\...\\title.basics.tsv'
 #full path or sql command (ex: mysql)
-sqlLocation = 'C:\\...\\mysql.exe'
+sqlLocation = 'C:\\Users\\...\\mysql.exe'
 sqlUser = 'root'
-sqlPass = '....'
+sqlPass = '.....'
 
 #set up / clean database
 sqlPrefix = sqlLocation + ' -u ' + sqlUser + ' -p' + sqlPass + ' -e '
@@ -18,20 +18,19 @@ os.system(sqlPrefix + "\"CREATE TABLE imdb.movies_info_raw (" \
           + "originalTitle VARCHAR(32) NOT NULL, " \
           + "isAdult INT NOT NULL, " \
           + "startYear INT NOT NULL, " \
-          + "endYear INT NOT NULL, " \
           + "runtimeMinutes INT NOT NULL, " \
           + "genres VARCHAR(32) NOT NULL, " \
           + "PRIMARY KEY (tconst)" \
           + ");\"")
 
 with open(imdbFile, encoding="utf8") as tsvfile:
-    reader = csv.reader(tsvfile, delimiter='\t')
     rowCount = 0
     rowsAdded = 0
+    reader = csv.reader(tsvfile, delimiter='\t')
     for row in reader:
         
         if rowCount % 100 == 0:
-            print("%s movies kept/added out of %s total movies --- file progress: %.4f %%" % (rowsAdded, rowCount, (rowCount/8388816)*100) )
+            print("%s movies kept/added out of %s total movies --- file progress: %.4f %%" % (rowsAdded, rowCount, (rowCount/4783534)*100) )
         
         #ignore 1st line of file
         rowCount += 1
@@ -39,30 +38,27 @@ with open(imdbFile, encoding="utf8") as tsvfile:
             continue
 
         #set up sql variables, fix invalid chars
-
+        # and throw out rows that don't have certain fields
         tconst = row[0]
         titleType = row[1].replace("'", "''").replace("\\", "\\\\")
+        if "movie" not in titleType:
+            continue
         primaryTitle = row[2].replace("'", "''").replace("\\", "\\\\")
         originalTitle = row[3].replace("'", "''").replace("\\", "\\\\")
         isAdult = row[4]
         startYear = row[5]
-        endYear = row[6]
-        runtimeMinutes = row[7]
-        genres = row[8].replace("'", "''").replace("\\", "\\\\")
-
-        #clean up
-        if "movie" not in titleType:
-            continue
-        if genres == '\\\\N':
-            continue
         if startYear == '\\N':
-            startYear = -1
-        if endYear == '\\N':
-            endYear = -1
+            continue
+        runtimeMinutes = row[7]
         if runtimeMinutes == '\\N':
             continue
+        genres = row[8].replace("'", "''").replace("\\", "\\\\")
+        if genres == '\\\\N':
+            continue
 
-        sqlCommand = sqlPrefix + "\"INSERT INTO imdb.movies_info_raw VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');\"" % (tconst,
-            primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
+        sqlCommand = sqlPrefix + "\"INSERT INTO imdb.movies_info_raw VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');\"" % (tconst,
+            primaryTitle, originalTitle, isAdult, startYear, runtimeMinutes, genres)
         os.system(sqlCommand)
         rowsAdded += 1
+
+print("Total rows added to table: %s" % rowsAdded)
