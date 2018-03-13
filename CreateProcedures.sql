48cf391d0acb2789e1f8094a65b1a713090c2f35
walkhh <36055790+walkhh@users.noreply.github.com>
2018-03-13 16:57:43 -0700

    Update CreateProcedures.sql

--- CreateProcedures.sql
+++ CreateProcedures.sql
@@ -1,88 +1,195 @@
-#DROP PROCEDURE getActors IF EXISTS;
-CREATE PROCEDURE getActors(IN Id INT)
+/*
+Quiz question procedures:
+ */
+
+# asks who was the lead star of a random movie
+DROP PROCEDURE IF EXISTS makeQuestionWhoWasLeadInRandMovie;
+CREATE PROCEDURE makeQuestionWhoWasLeadInRandMovie (OUT question VARCHAR(255),
+                                                    OUT a1 VARCHAR(255),
+                                                    OUT a2 VARCHAR(255),
+                                                    OUT a3 VARCHAR(255),
+                                                    OUT a4 VARCHAR(255))
   BEGIN
-    SELECT *
-    FROM ACTOR, (SELECT MA.ActorId
-                 FROM MOVIE_ACTOR MA
-                 WHERE MA.MovieId = Id) MA
-    WHERE ACTOR.ActorId = MA.ActorId;
+    SET @Min = getCustomMinNumVotes();
+    SET @randomMovie = getRandomMovieId();
+    SET question = CONCAT("Who was the lead actor in ", getMovieName(@randomMovie), "?");
+
+    SET @answerActorID =
+    (SELECT a.ActorID
+     FROM MovieActor ma
+       LEFT JOIN Movie m
+         ON ma.MovieID = m.MovieID
+       LEFT JOIN Actor a
+         ON ma.ActorID = a.ActorID
+     WHERE
+       m.MovieID = @randomMovie
+       AND m.numVotes > @Min
+       AND (m.startYear >= @MinMovieYear AND m.startYear <= @MaxMovieYear)
+     ORDER BY Importance
+     LIMIT 0,1
+    );
+
+    SET a1 = getActorName(@answerActorID);
+    SET a2 = getActorName(getRandomActorId());
+    SET a3 = getActorName(getRandomActorId());
+    SET a4 = getActorName(getRandomActorId());
   END;
 
-DELIMITER $
-CREATE PROCEDURE getMovies(IN Id INT)
-  BEGIN
-    SELECT *
-    FROM MOVIE M, (SELECT MA.MovieId
-                   FROM MOVIE_ACTOR MA
-                   WHERE MA.ActorId = Id) MA
-    WHERE M.Id = MA.MovieId;
-  END $
-DELIMITER ;
 
-#DROP PROCEDURE getMoviesByDecade;
-DELIMITER $
-CREATE PROCEDURE getMoviesByDecade(IN year INT)
+# asks what movie a random actor starred in
+DROP PROCEDURE IF EXISTS makeQuestionWhatMovieRandActorLeadIn;
+CREATE PROCEDURE makeQuestionWhatMovieRandActorLeadIn (OUT question VARCHAR(255),
+                                                       OUT a1 VARCHAR(255),
+                                                       OUT a2 VARCHAR(255),
+                                                       OUT a3 VARCHAR(255),
+                                                       OUT a4 VARCHAR(255))
   BEGIN
-    SELECT * FROM MOVIE M
-    WHERE M.StartYear >= year AND M.StartYear < year + 10
-    GROUP BY RAND()
-    LIMIT 10;
-  END $
-DELIMITER ;
+    SET @Min = getCustomMinNumVotes();
+    SET @movieAnswer = getRandomMovieId();
 
-DROP FUNCTION getBadActor;
-DELIMITER $
-CREATE FUNCTION getBadActor(Id INT, birth INT, gender CHAR(1))
-  RETURNS VARCHAR(512)
+    SET @TheActorID =
+    (SELECT a.ActorID
+     FROM MovieActor ma
+       LEFT JOIN Movie m
+         ON ma.MovieID = m.MovieID
+       LEFT JOIN Actor a
+         ON ma.ActorID = a.ActorID
+     WHERE
+       m.MovieID = @movieAnswer
+       AND m.numVotes > @Min
+       AND (m.startYear >= @MinMovieYear AND m.startYear <= @MaxMovieYear)
+     ORDER BY Importance
+     LIMIT 0,1
+    );
+
+    SET question = CONCAT("What movie did ", getActorName(@TheActorID), " star in?");
+    SET a1 = getMovieName(@movieAnswer);
+    SET a2 = getMovieName(getRandomMovieId());
+    SET a3 = getMovieName(getRandomMovieId());
+    SET a4 = getMovieName(getRandomMovieId());
+  END;
+
+
+# Creates question asking what random movie 2 or more random actors starred in
+DROP PROCEDURE IF EXISTS makeQuestionWhatMovieStarsTheseActors;
+CREATE PROCEDURE makeQuestionWhatMovieStarsTheseActors(OUT question VARCHAR(255),
+                                                       OUT a1 VARCHAR(255),
+                                                       OUT a2 VARCHAR(255),
+                                                       OUT a3 VARCHAR(255),
+                                                       OUT a4 VARCHAR(255))
   BEGIN
-    RETURN(SELECT A.FullName FROM ACTOR A
-    WHERE ActorId != Id AND A.Gender = gender
-          AND  ABS(A.BirthYear - birth) <= 8
-    GROUP BY RAND()
-           LIMIT 1);
-  END $
-DELIMITER ;
+    SET @Min = getCustomMinNumVotes();
 
-DELIMITER $
-CREATE FUNCTION getBadMovie(Id INT, year INT)
-  RETURNS VARCHAR(512)
+    SELECT m.MovieID, GROUP_CONCAT(a.primaryName SEPARATOR ', and ') INTO @TheMovie, @TheActors
+    FROM Movie m
+      LEFT JOIN MovieActor ON m.MovieID = MovieActor.MovieID
+      LEFT JOIN Actor a ON a.ActorID = MovieActor.ActorID
+    WHERE m.numVotes > @Min
+          AND (m.startYear >= @MinMovieYear AND m.startYear <= @MaxMovieYear)
+    GROUP BY m.MovieID
+    having count(distinct a.ActorID) >= @MinRelatedness
+    ORDER BY RAND()
+    LIMIT 0,1
+    ;
+
+    SET question = CONCAT("What movie did the actors ", @TheActors, " star in?");
+    SET a1 = getMovieName(@TheMovie);
+    SET a2 = getMovieName(getRandomMovieId());
+    SET a3 = getMovieName(getRandomMovieId());
+    SET a4 = getMovieName(getRandomMovieId());
+  END;
+
+
+
+# Creates question asking what random actor starred in 2 or more random movies
+DROP PROCEDURE IF EXISTS makeQuestionWhatActorStarredInTheseMovies;
+CREATE PROCEDURE makeQuestionWhatActorStarredInTheseMovies(OUT question VARCHAR(255),
+                                                           OUT a1 VARCHAR(255),
+                                                           OUT a2 VARCHAR(255),
+                                                           OUT a3 VARCHAR(255),
+                                                           OUT a4 VARCHAR(255))
   BEGIN
-    RETURN(SELECT M.PrimaryTitle FROM MOVIE M
-    WHERE M.StartYear = year AND M.Id != Id
-    GROUP BY RAND()
-           LIMIT 1);
-  END $
-DELIMITER ;
+    SET @Min = getCustomMinNumVotes();
 
-DELIMITER $
-CREATE PROCEDURE getMovie(IN Id INT)
+    SELECT a.ActorID, GROUP_CONCAT(m.primaryTitle SEPARATOR ', and ') INTO @TheActor, @TheMovies
+    FROM Actor a
+      LEFT JOIN MovieActor ON a.ActorID = MovieActor.ActorID
+      LEFT JOIN Movie m ON m.MovieID = MovieActor.MovieID
+    WHERE m.numVotes > @Min
+          AND (m.startYear >= @MinMovieYear AND m.startYear <= @MaxMovieYear)
+    GROUP BY a.ActorID
+    having count(distinct m.MovieID) >= @MinRelatedness
+    ORDER BY RAND()
+    LIMIT 0,1
+    ;
+
+    SET question = CONCAT("What actor starred in the movies ", @TheMovies, "?");
+    SET a1 = getActorName(@TheActor);
+    SET a2 = getActorName(getRandomActorId());
+    SET a3 = getActorName(getRandomActorId());
+    SET a4 = getActorName(getRandomActorId());
+  END;
+
+
+# Create question asking what year a random movie was made in
+DROP PROCEDURE IF EXISTS createQuestionWhatYearWasRandMovie;
+CREATE PROCEDURE createQuestionWhatYearWasRandMovie(OUT question VARCHAR(255),
+                                                    OUT a1 VARCHAR(255),
+                                                    OUT a2 VARCHAR(255),
+                                                    OUT a3 VARCHAR(255),
+                                                    OUT a4 VARCHAR(255))
   BEGIN
-    SELECT * FROM MOVIE
-    WHERE MOVIE.Id = Id;
-  END $
-DELIMITER ;
-
-DELIMITER $
-CREATE PROCEDURE getActor(IN Id INT)
-  BEGIN
-    SELECT * FROM ACTOR
-    WHERE ACTOR.ActorId = Id;
-  END $
-DELIMITER ;
-
-DROP PROCEDURE  getMovieYearQuestion;
-DELIMITER $
-CREATE PROCEDURE getMovieYearQuestion(OUT question VARCHAR(255), IN name VARCHAR(128),
-                                      IN a1 INT, OUT a2 INT, OUT a3 INT, OUT a4 INT)
-  BEGIN
-    SET question = CONCAT("What year was ", name, " made?");
-
+    SET @randMovie = getRandomMovieId();
+    SET question = CONCAT("What year was ", getMovieName(@randMovie), " made?");
+    SET a1 = getMovieYear(@randMovie);
     SET a2 = getRandomYear(a1);
     Set a3 = getRandomYear(a1);
     Set a4 = getRandomYear(a1);
-  END $
-DELIMITER ;
+  END;
+CALL createQuestionWhatYearWasRandMovie(@q, @a, @w1, @w2, @w3);
+SELECT @q, @a, @w1, @w2, @w3;
 
+
+
+/*
+Utility procedures:
+ */
+
+# Pick random movie id
+DROP FUNCTION IF EXISTS getRandomMovieId;
+CREATE FUNCTION getRandomMovieId()
+  RETURNS INT
+  BEGIN
+    SET @Min = getCustomMinNumVotes();
+    SET @RandomMovie =
+    (SELECT MovieID
+     FROM Movie
+     WHERE
+       numVotes > @Min
+       AND (startYear >= @MinMovieYear AND startYear <= @MaxMovieYear)
+     # AND NOT MovieID = notID
+     ORDER BY RAND()
+     LIMIT 1);
+    RETURN @RandomMovie;
+  END;
+
+
+# Pick random actor id
+DROP FUNCTION IF EXISTS getRandomActorId;
+CREATE FUNCTION getRandomActorId()
+  RETURNS INT
+  BEGIN
+    SET @RandomMovie =
+    (SELECT ActorID
+     FROM Actor
+     #WHERE NOT ActorID = notID
+     ORDER BY RAND()
+     LIMIT 1);
+    RETURN @RandomMovie;
+  END;
+
+
+# Gets a random year
 DROP FUNCTION getRandomYear;
 DELIMITER $
 CREATE FUNCTION getRandomYear(a1 INT)
@@ -91,3 +198,50 @@ CREATE FUNCTION getRandomYear(a1 INT)
     RETURN a1 - 5 + FLOOR(1 + RAND() * 10);
   END $
 DELIMITER ;
+
+
+/*
+gets the minimum number a of votes a movie can have when finding a random movie
+based off the setting @DifficultyPercent
+ */
+DROP FUNCTION IF EXISTS getCustomMinNumVotes;
+CREATE FUNCTION getCustomMinNumVotes()
+  RETURNS FLOAT
+  BEGIN
+    SET @highestNumVotes = (SELECT MAX(numVotes) FROM Movie) - @MinNumVotes;
+    SET @easynessPercent = 100.00 - @DifficultyPercent;
+    SET @customHighestNumVotes = @highestNumVotes * @easynessPercent / 100.00;
+    RETURN @customHighestNumVotes + @MinNumVotes;
+  END;
+
+
+
+# Gets the primary name an actor based on its ID
+DROP FUNCTION IF EXISTS getActorName;
+CREATE FUNCTION getActorName(AID INT)
+  RETURNS VARCHAR(128)
+  BEGIN
+    RETURN (SELECT A.primaryName FROM Actor A
+    WHERE A.ActorId = AID);
+  END;
+
+
+# Gets the year of a movie based on its ID
+DROP FUNCTION IF EXISTS getMovieYear;
+CREATE FUNCTION getMovieYear(MID INT)
+  RETURNS INT
+  BEGIN
+    RETURN (SELECT M.startYear
+            FROM Movie M
+    WHERE M.MovieId = MID);
+  END;
+
+
+# Gets the primary name a movie based on its ID
+DROP FUNCTION IF EXISTS getMovieName;
+CREATE FUNCTION getMovieName(MID INT)
+  RETURNS VARCHAR(512)
+  BEGIN
+    RETURN (SELECT M.primaryTitle FROM Movie M
+    WHERE M.MovieId = MID);
+  END;
