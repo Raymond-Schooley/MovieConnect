9016d1d967a72e489e420db40bfd18a870f01087
igork15 <32179335+igork15@users.noreply.github.com>
2018-03-13 17:29:15 -0700

    Update inc.dbc.php

--- php/inc.dbc.php
+++ php/inc.dbc.php
@@ -1,10 +1,10 @@
 <?php
 
 	function get_connection() {
-		$userid   = 'ravschoo'; //Change this to yours
-		$password = 'Gruntzle1'; //Change this to yours
+		$userid   = 'yourid'; //Change this to yours
+		$password = 'yourpass'; //Change this to yours
 		$host     = 'cssgate.insttech.washington.edu';
-		$dbname   = 'ravschoo'; //Change this to yours
+		$dbname   = 'yourname'; //Change this to yours
 		//jdbc:mysql://localhost:3306/tcss445
 		$dsn = 'mysql:host='.$host. ';dbname='. $dbname;
 		
